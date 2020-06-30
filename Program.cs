20d08647eeaa03ece19305a6ce76f4fdfa4abdf1
cirno <metroyuan@gmail.com>
2020-06-30 16:16:07 +0800

    removed unused using

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using CommandDotNet;
-using ConsoleTables;
-using GoPractice.MyUtil;
-using System.Collections.Generic;
-using System.Net;
+﻿using CommandDotNet;
 
 namespace GoPracticeCli
 {
